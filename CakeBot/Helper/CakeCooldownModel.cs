5f2d518436928ca76f614703347ed4642cc9af92
CakeAndBanana <admin@cakeandbanana.nl>
2019-05-25 01:49:25 +0900

    Add Cooldown model

---
+++ CakeBot/Helper/CakeCooldownModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CakeBot.Helper
+{
+    public class CakeCooldownModel
+    {
+        public class CooldownModel
+        {
+            public ulong Id { get; set; }
+            public DateTime CooldownDateTime { get; set; }
+        }
+    }
+}
