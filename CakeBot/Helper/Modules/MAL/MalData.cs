9de6d53849af4a1d0b6c126b1b162234c052df78
CakeAndBanana <admin@cakeandbanana.nl>
2019-05-26 01:39:12 +0900

    Remove unused Enum

--- CakeBot/Helper/Modules/MAL/MalData.cs
+++ CakeBot/Helper/Modules/MAL/MalData.cs
@@ -2,11 +2,7 @@
 using CakeBot.Helper.Modules.MAL.Model;
 using Newtonsoft.Json;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CakeBot.Helper.Modules.MAL
 {
@@ -16,10 +12,12 @@ namespace CakeBot.Helper.Modules.MAL
         {
             return JsonConvert.DeserializeObject<MALModel>(url);
         }
-        static string baseUrl = "https://api.jikan.moe/v3";
+
+        private const string BaseUrl = "https://api.jikan.moe/v3";
+
         public static MALModel GetRandomAnimeByCat(int genre_id)
         {
-            var url = $"{baseUrl}/genre/anime/{genre_id}";
+            var url = $"{BaseUrl}/genre/anime/{genre_id}";
             try
             {
                 url = new WebClient().DownloadString(url);
@@ -34,7 +32,7 @@ namespace CakeBot.Helper.Modules.MAL
         }
         public static MALModel GetRandomMangaByCat(int genre_id)
         {
-            var url = $"{baseUrl}/genre/manga/{genre_id}";
+            var url = $"{BaseUrl}/genre/manga/{genre_id}";
 
             try
             {
@@ -50,7 +48,7 @@ namespace CakeBot.Helper.Modules.MAL
         }
         public static MALModel SearchAnime(string name)
         {
-            var url = $"{baseUrl}/search/anime?q={name}&page=1";
+            var url = $"{BaseUrl}/search/anime?q={name}&page=1";
 
             try
             {
@@ -65,7 +63,7 @@ namespace CakeBot.Helper.Modules.MAL
         }
         public static MALModel SearchManga(string name)
         {
-            var url = $"{baseUrl}/search/manga?q={name}&page=1";
+            var url = $"{BaseUrl}/search/manga?q={name}&page=1";
 
             try
             {
@@ -78,49 +76,5 @@ namespace CakeBot.Helper.Modules.MAL
 
             return JsonConvert.DeserializeObject<MALModel>(url);
         }
-    }
-
-    public enum Genres
-    {
-        Action = 1,
-        Adventure = 2,
-        Cars = 3,
-        Comedy = 4,
-        Dementia = 5,
-        Demons = 6,
-        Mystery = 7,
-        Drama = 8,
-        Ecchi = 9,
-        Fantasy = 10,
-        Game = 11,
-        Hentai = 12,
-        Historical = 13,
-        Horror = 14,
-        Kids = 15,
-        Magic = 16,
-        Martial_Arts = 17,
-        Mecha = 18,
-        Music = 19,
-        Parody = 20,
-        Samurai = 21,
-        Romance = 22,
-        School = 23,
-        Sci_Fi = 24,
-        Shoujo = 25,
-        Shoujo_Ai = 26,
-        Shounen = 27,
-        Shounen_Ai = 28,
-        Space = 29,
-        Sports = 30,
-        Super_Power = 31,
-        Vampire = 32,
-        Yaoi = 33,
-        Yuri = 34,
-        Harem = 35,
-        Slice_Of_Life = 36,
-        Supernatural = 37,
-        Military = 38,
-        Police = 39,
-        Psychological = 40
     }
 }
