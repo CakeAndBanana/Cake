016f074759baea7865cf3250d01ac0a0227152d7
CakeAndBanana <admin@cakeandbanana.nl>
2019-05-31 20:41:04 +0900

    Added Cake.Modules project with base

---
+++ Cake.Modules/Modules/CustomBaseModule.cs
@@ -0,0 +1,9 @@
+﻿using Discord.Commands;
+
+namespace Cake.Modules.Modules
+{
+    public class CustomBaseModule : ModuleBase
+    {
+        // Add things when you use them multiple times.
+    }
+}
