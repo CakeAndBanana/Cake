27c32a8a22609dd044913f38194f586b9d22630a
CakeAndBanana <admin@cakeandbanana.nl>
2019-06-13 23:52:37 +0900

    Rename

--- Cake.Database/Queries/UserQueries.cs
+++ Cake.Database/Queries/UserQueries.cs
@@ -8,7 +8,7 @@ namespace Cake.Database.Queries
 {
     public class UserGueries
     {
-        public async static Task<List<CakeUser>> GetAllUsers()
+        public async static Task<List<CakeUser>> GetAdallUsers()
         {
             using (var db = new CakeDb())
             {
