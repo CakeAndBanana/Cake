c6f13ed1c45da1f5b3c0c84e46f6e51a587b7838
CakeAndBanana <admin@cakeandbanana.nl>
2019-06-18 17:52:48 +0900

    Added osuModule and osuService.

---
+++ Cake.Core/Discord/Modules/OsuModule.cs
@@ -0,0 +1,49 @@
+﻿using System.Threading.Tasks;
+using Cake.Core.Discord.Services;
+using Discord.Commands;
+
+namespace Cake.Core.Discord.Modules
+{
+    [Name("osu!")]
+    [Group("osu")]
+    [Alias("o")]
+    class OsuModule : CustomBaseModule
+    {
+        private readonly OsuService _service;
+
+        public OsuModule(OsuService service)
+        {
+            _service = service;
+            _service.SetBaseModule(this);
+        }
+
+        [Command("set")]
+        [Summary("osu set")]
+        [Alias("s")]
+        [Remarks("Binds your osu account to your discord user.")]
+        public async Task SetAccount([Remainder]string username)
+        {
+            await _service.SetAccount(username);
+        }
+
+        [Command("profile")]
+        [Summary("osu profile (?user)")]
+        [Alias("u", "p")]
+        [Remarks("Gets profile of current user or given user.")]
+        public async Task GetProfile([Remainder]string username)
+        {
+            int osuId = 0;
+
+            if (username == null)
+            {
+                //get account
+            }
+            else
+            {
+                //get id here from username   
+            }
+
+            await _service.GetProfile(osuId);
+        }
+    }
+}
---
+++ Cake.Core/Discord/Services/OsuService.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+
+namespace Cake.Core.Discord.Services
+{
+    class OsuService : CustomBaseService
+    {
+        public async Task SetAccount(string username)
+        {
+            await SendMessageAsync($"``SetAccount (setaccounthere)``");
+        }
+        public async Task GetProfile(int id)
+        {
+            await SendMessageAsync($"``GetProfile (getaccounthere)``");
+        }
+    }
+}
