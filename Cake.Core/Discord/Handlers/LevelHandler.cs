6a2ba188e461ae6fa795e842b81af8309d63ca06
CakeAndBanana <admin@cakeandbanana.nl>
2019-08-14 02:30:52 +0200

    Added new class for level/xp system.

---
+++ Cake.Core/Discord/Handlers/LevelHandler.cs
@@ -0,0 +1,24 @@
+﻿using Cake.Database.Models;
+using System;
+using System.Threading.Tasks;
+
+namespace Cake.Core.Discord.Handlers
+{
+    public class LevelHandler
+    {
+        public async Task GiveExpToUser(CakeUser user, int expToGive)
+        {
+            throw new NotImplementedException("");
+        }
+
+        private async Task IncrementLevel(CakeUser user, int amount = 1)
+        {
+            throw new NotImplementedException("");
+        }
+
+        private async Task DecreaseLevel(CakeUser user, int amount = 1)
+        {
+            throw new NotImplementedException("");
+        }
+    }
+}
