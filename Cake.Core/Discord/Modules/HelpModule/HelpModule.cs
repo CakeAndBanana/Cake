d1b5d26aee0813710d3011feae8111380bbc54eb
Kaynn Cahya <kaynncahya@outlook.com>
2019-08-18 01:00:29 +0900

    Subscribed the helpbook object to MessageReactionHandler event

--- Cake.Core/Discord/Modules/HelpModule/HelpModule.cs
+++ Cake.Core/Discord/Modules/HelpModule/HelpModule.cs
@@ -2,6 +2,7 @@
 using System.Threading.Tasks;
 using Cake.Core.Discord.Attributes;
 using Cake.Core.Discord.Embed.Builder;
+using Cake.Core.Discord.Handlers;
 using Cake.Core.Discord.Services;
 using Discord.Commands;
 
@@ -28,7 +29,9 @@ namespace Cake.Core.Discord.Modules
         {
             List<CakeEmbedBuilder> helpPages = await _service.FetchAllCommandInfoAsPages(_commandService);
 
+            HelpBook helpBook = new HelpBook(helpPages, Context.Message.Id);
 
+            MessageReactionHandler.AddMessageReactionHandle(helpBook);
 
             // TODO: Command searching filter
         }
