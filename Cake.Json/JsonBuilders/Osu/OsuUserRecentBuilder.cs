7ca817693be10f6b3db40e4ed784234c2a7b3103
Pim Wingelaar <48980200+CakeAndBanana@users.noreply.github.com>
2020-02-03 22:23:52 +0100

    Changed choked stuff

--- Cake.Json/JsonBuilders/Osu/OsuUserRecentBuilder.cs
+++ Cake.Json/JsonBuilders/Osu/OsuUserRecentBuilder.cs
@@ -54,7 +54,7 @@ namespace Cake.Json.CakeBuilders.Osu
 
                 item.hitted = item.countkatu + item.countgeki + item.count300 + item.count100 + item.count50 +
                               item.countmiss;
-                if(item.maxcombo >= (beatmapData.GetMaxCombo() - (item.count100 + item.count50)))
+                if(item.maxcombo <= (beatmapData.GetMaxCombo() - (item.count100 + item.count50)))
                     item.choked = true;
                     else if (item.countmiss > 0)
                     item.choked = true;
