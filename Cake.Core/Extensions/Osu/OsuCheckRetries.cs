c19b823b53312ba6930336fa7417051d8b82048f
Pim Wingelaar <48980200+CakeAndBanana@users.noreply.github.com>
2020-02-03 22:28:15 +0100

    Moved extension to seperate classes

---
+++ Cake.Core/Extensions/Osu/OsuCheckRetries.cs
@@ -0,0 +1,35 @@
+﻿using Cake.Json.CakeBuilders.Osu;
+
+namespace Cake.Core.Extensions.Osu
+{
+    public class OsuCheckRetries
+    {
+        public static int Tries(string mode, string userid, int beatmapid)
+        {
+            int count = 0;
+
+            var recentBuilder = new OsuUserRecentBuilder()
+            {
+                Mode = mode,
+                Limit = "20",
+                UserId = userid
+            };
+
+            var result = recentBuilder.Execute(false, true);
+
+            foreach (var recent in result)
+            {
+                if (beatmapid == recent.beatmap_id)
+                {
+                    count++;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            return count;
+        }
+    }
+}
